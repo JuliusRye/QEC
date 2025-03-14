61c21a507f6805c1ca02822243da4457fd014959
JuliusRye <juliusrye@gmail.com>
2025-03-14 11:45:44 +0100

    Merged the environments into one python file

---
+++ Modules/environment.py
@@ -0,0 +1,171 @@
+from quantum_error_correction_code import SurfaceCode
+from neural_network import CNNDual
+from perfect_maximum_likelihood_decoder import PMLD
+from functools import partial
+from jax import random, jit, vmap
+import jax.numpy as jnp
+from abc import ABC, abstractmethod
+
+
+class EnvironmentBase(ABC):
+
+    def __init__(
+        self,
+        noise_model: jnp.ndarray,
+        code: SurfaceCode,
+    ):
+        self.num_qubits = code.hx_original.shape[1]
+        self.noise_model = noise_model
+        self.code = code
+
+    def reset(
+        self,
+        key,
+    ):
+        """
+        Reset the system to the non-deformed quantum error correction code.
+        """
+        state = jnp.zeros(shape=(self.num_qubits), dtype=jnp.int32)
+        score, key = self._score_state(key, state)
+        return state, score, key
+
+    @abstractmethod
+    def _score_state(
+        self,
+        key,
+        state: jnp.ndarray,
+    ):
+        """
+        Calculates the score of the state
+
+        returns: score, key
+        """
+        pass
+
+    def reward(
+        self,
+        key,
+        current_score: float,
+        new_state: jnp.ndarray,
+    ):
+        new_score, key = self._score_state(key, new_state)
+        reward = new_score - current_score
+        return reward, new_score, key
+
+
+class EnvironmentCNN(EnvironmentBase):
+
+    def __init__(
+        self,
+        model: CNNDual,
+        model_params,
+        noise_model: jnp.ndarray,
+        code: SurfaceCode,
+        shots: int
+    ):
+        """
+        The environment the RL-agent will interact with. 
+        This environment uses a CNN to estimate the logical error rate of a given deformation. 
+        This logical error rate is in turn used to score the state 
+        and calculate the reward the RL-agent will recieve.
+
+        model: The CNN that will be used to decode errors on a given deformation
+
+        model_params: The finetuned parameters used by the model after training
+
+        noise_model: The Pauli error probabilities used to generate errors for the decoder
+
+        code: The quantum error correction code that is being deformed
+
+        shots: The number of samples given to the CNN for the estimation of the logical error rate
+        """
+        super().__init__(noise_model, code)
+        self.decoder = model
+        self.params = model_params
+        self.shots = shots
+
+    @partial(jit, static_argnames=("self"))
+    def _score_state(
+        self,
+        key,
+        state: jnp.ndarray,
+    ):
+        """
+        Calculates the score of the state corresponding to number of qec cycles that can be performed before going below 99% logical fidelity.
+
+        state: Current Clifford deformation
+        """
+        keys = random.split(key, num=self.shots+1)
+
+        # Generate syndrome data for the deformation
+        parity_info = self.code.deformation_parity_info(state)
+        errors = vmap(
+            self.code.error,
+            in_axes=(0, None),
+            out_axes=0
+        )(keys[:-1], self.noise_model)
+        syndrome_img, logicals = vmap(
+            self.code.syndrome_img,
+            in_axes=(0, None),
+            out_axes=0
+        )(errors, parity_info)
+        deformation_image = self.code.deformation_image(state)
+
+        predictions = self.decoder.apply_batch(
+            self.params,
+            syndrome_img[:, None, :, :],
+            deformation_image,
+        )
+
+        error_rate = jnp.any(
+            logicals != (predictions > 0),
+            axis=1
+        ).mean()
+
+        # Number of qec cycles that can be performed while maintaining above 99% logical fidelity
+        score = -jnp.log(.99) / error_rate
+
+        return score, keys[-1]
+
+
+class EnvironmentPML(EnvironmentBase):
+
+    def __init__(
+        self,
+        noise_model: jnp.ndarray,
+        code: SurfaceCode
+    ):
+        """
+        The environment the RL-agent will interact with. 
+        This environment uses the PML-decoder to give the logical error rate of a given deformation. 
+        This logical error rate is in turn used to score the state 
+        and calculate the reward the RL-agent will recieve.
+
+        noise_model: The Pauli error probabilities used to generate errors for the decoder
+
+        code: The quantum error correction code that is being deformed
+        """
+        super().__init__(noise_model, code)
+
+    @partial(jit, static_argnames=("self"))
+    def _score_state(
+        self,
+        key,  # This argument is only for consistency between environments classes
+        state: jnp.ndarray,
+    ):
+        """
+        Calculates the score of the state corresponding to number of qec cycles that can be performed before going below 99% logical fidelity.
+
+        state: Current Clifford deformation
+        """
+
+        error_rate = PMLD(
+            self.code,
+            self.noise_model,
+            self.code.deformation_parity_info(state)
+        ).exact_logical_error_rate()
+
+        # Number of qec cycles that can be performed while maintaining above 99% logical fidelity
+        score = -jnp.log(.99) / error_rate
+
+        return score, key
